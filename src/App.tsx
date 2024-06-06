df0901923583ed24de80e14148d51ca9cd3fd585
Daniel Austin <danielaustin718@gmail.com>
2024-06-06 14:55:57 +0100

    Basic dashboard.

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import keypair from 'keypair';
+import { useForm } from 'react-hook-form';
+import { WalletContext, WalletContextProvider } from './wallet/WalletContext';
+import { useContext } from 'react';
+import moment from 'moment';
+import { QRCodeCanvas } from 'qrcode.react';
+
+function CreateWallet() {
+  const walletCtx = useContext(WalletContext);
+
+  const createWallet = () => {
+    const pair = keypair({ bits: 512 });
+
+    const clean = (key: string) =>
+      key
+        .replace('-----BEGIN RSA PRIVATE KEY-----', ' ')
+        .replace('-----END RSA PRIVATE KEY-----', '')
+        .replace('-----BEGIN RSA PUBLIC KEY-----', ' ')
+        .replace('-----END RSA PUBLIC KEY-----', '')
+        .replace(/\s+/g, '')
+        .trim();
+
+    const privateKey = clean(pair['private']);
+    const address = clean(pair['public']);
+
+    walletCtx.addWallet({
+      address,
+      privateKey,
+      balance: 0
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={() => createWallet()}>Create Wallet</button>
+    </div>
+  )
+}
+
+function TransferFunds() {
+  const walletCtx = useContext(WalletContext);
+
+  const { register, handleSubmit } = useForm();
+
+  const transferFunds = (formData: any) => {
+    const payload = formData;
+    payload.from = walletCtx.wallet.address;
+    payload.signature = 'fjejfijfjefefe';
+    payload.timestamp = moment().valueOf();
+
+    console.log(payload);
+  }
+
+  return (
+    <div className="shadow-lg">
+      <form onSubmit={handleSubmit(transferFunds)}>
+        <label>Enter address here:</label>
+        <input type="text" {...register('to', { required: true })} />
+
+        <label>Amount</label>
+        <input type="number" step=".00001" {...register('amount', { required: true })} />
+
+        <button type="submit" className="bg-blue-800 text-sm rounded-md px-3 py-2 font-semibold shadow-sm text-white">Send</button>
+      </form>
+    </div>
+  )
+}
+
+function MyWallet({ ...props }) {
+  const walletCtx = useContext(WalletContext);
+
+  return (
+    <div {...props}>
+      <div>
+        <QRCodeCanvas value={walletCtx.wallet.address} />
+      </div>
+      <div>
+        <div>{walletCtx.wallet.address}</div>
+      </div>
+    </div>
+  );
+}
+
+function WalletBalance({ ...props }) {
+  const walletCtx = useContext(WalletContext);
+
+  return (
+    <div {...props}>
+      <div>BALANCE</div>
+      <div className="text-3xl font-bold">SMPL {walletCtx.wallet.balance}</div>
+      <div>
+        <button className="bg-blue-800 text-sm rounded-md px-3 py-2 font-semibold shadow-sm text-white">Buy</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <WalletContextProvider>
+      <>
+        <div className="container mx-auto">
+          <h1>My Wallet</h1>
+          <CreateWallet />
+        </div>
+        <div className="container mx-auto">
+          <div className="flex flex-row shadow-lg">
+            <WalletBalance className="basis-1/2" />
+            <MyWallet className="basis-1/2" />
+          </div>
+          <TransferFunds />
+        </div>
+      </>
+    </WalletContextProvider>
+  )
+}
+
+export default App
---
+++ src/wallet/WalletContext.tsx
@@ -0,0 +1,43 @@
+import { ReactElement, createContext, useEffect, useState } from "react";
+import _cloneDeep from 'lodash.clonedeep';
+
+interface IWallet {
+    address: string;
+    privateKey: string;
+    balance: 0;
+}
+
+const defaultWallet: IWallet = {
+    address: '',
+    privateKey: '',
+    balance: 0
+};
+
+const WalletContext = createContext({
+    wallet: _cloneDeep(defaultWallet),
+    addWallet: (newWallet: IWallet) => {}
+});
+
+const WalletContextProvider = ({ children }: { children: ReactElement }) => {
+    const [wallet, setWallet] = useState(_cloneDeep(defaultWallet));
+
+    // Prefetch wallets from localstorage
+    useEffect(() => {
+        const localWallet = localStorage.getItem('simple-wallet');
+        if (localWallet) {
+            setWallet(JSON.parse(localWallet));
+        }
+    }, []);
+
+    const addWallet = (newWallet: IWallet) => {
+        setWallet(newWallet);
+    }
+
+    return (
+        <WalletContext.Provider value={{ wallet, addWallet }}>
+            {children}
+        </WalletContext.Provider>
+    )
+}
+
+export { WalletContext, WalletContextProvider };
